24314831a038ce309476bf2501b98490fbeacf58
surya <suryanarayana@goorulearning.org>
2014-05-20 19:13:19 +0530

    "Users should be able to add tags to existing resources"

---
+++ src/main/java/org/ednovo/gooru/client/uc/SlideBarView.java
@@ -0,0 +1,14 @@
+package org.ednovo.gooru.client.uc;
+
+import com.google.gwt.widgetideas.client.SliderBar;
+
+
+public class SlideBarView extends SliderBar {
+
+	public SlideBarView(double minValue, double maxValue) {
+		super(minValue, maxValue);
+	
+	}
+
+	
+}
