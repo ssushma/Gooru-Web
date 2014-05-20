99cf6c644158807936544f54ee119d82ba7dacbd
Mitra <janamithra@goorulearning.org>
2014-05-20 10:59:20 +0530

    "To set the General info details by using API"

--- src/main/java/org/ednovo/gooru/client/mvp/search/SearchInfoWidget.java
+++ src/main/java/org/ednovo/gooru/client/mvp/search/SearchInfoWidget.java
@@ -3,18 +3,37 @@
  */
 package org.ednovo.gooru.client.mvp.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.ednovo.gooru.client.SimpleAsyncCallback;
+import org.ednovo.gooru.client.gin.AppClientFactory;
+import org.ednovo.gooru.client.mvp.play.collection.info.ResourceInfoView.MouseOutHideToolTip;
+import org.ednovo.gooru.client.mvp.play.collection.info.ResourceInfoView.MouseOverShowStandardToolTip;
+import org.ednovo.gooru.client.uc.ToolTipPopUp;
+import org.ednovo.gooru.shared.model.content.CollectionItemDo;
+import org.ednovo.gooru.shared.model.content.LicenseDo;
+import org.ednovo.gooru.shared.model.search.ResourceSearchResultDo;
 import org.ednovo.gooru.shared.util.MessageProperties;
+import org.ednovo.gooru.shared.util.StringUtil;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- * @author ibc
+ * @author mitraJ
  *
  */
 public class SearchInfoWidget extends Composite implements MessageProperties{
@@ -44,6 +63,15 @@ public class SearchInfoWidget extends Composite implements MessageProperties{
 	authorPanel,gooruSubjectPanel,eduUseType,keyWordsPanel,keywordsInfo,readingLevelType,gooruCourseInfo,accessModeType,mediaFeatureType,accessibilityAPIType,dKnowledgeType,
 	momentsoflearningPanel,momentsoflearningType,thumbnailurlValue;
 	
+	@UiField FlowPanel licenceContainer,standardsInfoConatiner;
+	
+	ToolTipPopUp toolTip ; 
+	
+	private static final String ALL_GRADES = "ALL GRADES";
+	
+	private ResourceSearchResultDo searchResultDo;
+	private CollectionItemDo collectionItemDo;
+	
 	/**
 	 * Because this class has a default constructor, it can
 	 * be used as a binder template. In other words, it can be used in other
@@ -58,6 +86,12 @@ public class SearchInfoWidget extends Composite implements MessageProperties{
 	public SearchInfoWidget() {
 		initWidget(uiBinder.createAndBindUi(this));
 		
+		setGenealInfoData();
+		
+		
+	}
+	
+	private void setGenealInfoData() {
 		publisherText.setText(GL0566);
 		courseText.setText(GL0616);
 		legalText.setText(GL0730+ ""+GL_SPL_SEMICOLON);
@@ -65,11 +99,294 @@ public class SearchInfoWidget extends Composite implements MessageProperties{
 //		resourceInfoText.setText(GL0621);
 		gradeTitle.setText(GL0325+ ""+GL_SPL_SEMICOLON);
 		originalUrlTitle.setText(GL0976+ ""+GL_SPL_SEMICOLON);
-
 		generalLbl.setText(GL1708);
 		resourceInfoLbl.setText(GL1716);
 		educationallLbl.setText(GL1720);
+		legalText.setText("License:");
+	}
+
+	/**
+	 * Assign {@link ResourceSearchResultDo} instance
+	 * 
+	 * @param searchResultDo
+	 *            instance of {@link ResourceSearchResultDo}
+	 */
+	public void setData(ResourceSearchResultDo searchResultDo) {
+		this.searchResultDo = searchResultDo;
+	}
+	
+	/**
+	 * @param collectionItemDo
+	 *            instance of {@link collectionItemDo}
+	 * 
+	 */
+	
+	public void setData(CollectionItemDo collectionItemDo) {
+		this.collectionItemDo =collectionItemDo;
+	}
+	
+	public void setData(){
+		System.out.println("gooruid::"+searchResultDo.getGooruOid());
+		AppClientFactory.getInjector().getPlayerAppService().getResourceCollectionItem(null, searchResultDo.getGooruOid(), null, new SimpleAsyncCallback<CollectionItemDo>() {
+
+			@Override
+			public void onSuccess(CollectionItemDo result) {
+				// TODO Auto-generated method stub
+				System.out.println("onSucess:"+result.getGrade());
+				
+				setResourceLicenceLogo(result.getResource().getAssetURI(), result.getResource().getLicense());
+				
+			}
+		});
+	}
+	
+	/**
+	 * To set Resource License
+	 * @param assetUrl 
+	 * @param licenseDo instance of {@link LicenseDo}
+	 */
+	public void setResourceLicenceLogo(String assetUrl,LicenseDo licenseDo){
+		if(licenseDo!=null){
+			if(licenseDo.getIcon()!=null&&!licenseDo.getIcon().trim().equals("")){
+				Image image=new Image();
+				image.setUrl(assetUrl+licenseDo.getIcon());
+				//image.addMouseOverHandler(new MouseOverShowStandardToolTip(licenseDo.getDefinition()));
+				image.addMouseOverHandler(new MouseOverShowStandardToolTip(licenseDo.getCode()+"         "+licenseDo.getName()));
+				image.addMouseOutHandler(new MouseOutHideToolTip());
+				licenceContainer.setVisible(true);
+				rightsLogoContainer.clear();
+				rightsLogoContainer.add(image);
+			}
+			else{
+				licenceContainer.setVisible(false);
+				rightsLogoContainer.clear();
+				//rightsLogoContainer.add(setText(GL0977));
+			}
+		}else{
+			licenceContainer.setVisible(false);
+			rightsLogoContainer.clear();
+			//rightsLogoContainer.add(setText(GL0977));
+		}		
+	}	
+	
+	
+	public class MouseOverShowStandardToolTip implements MouseOverHandler
+	{
+		String desc=null;
+		
+		public MouseOverShowStandardToolTip(String description) {
+			this.desc = description;
+		}
+		
+		@Override
+		public void onMouseOver(MouseOverEvent event) {
+			toolTip = new ToolTipPopUp(desc, (event.getRelativeElement().getAbsoluteLeft()-83),(event.getRelativeElement().getAbsoluteTop()+22));
+			toolTip.setStyleName("");
+			toolTip.show();
+			toolTip.getElement().getStyle().setZIndex(99999);
+		}
+	}
+	
+	public class MouseOutHideToolTip implements MouseOutHandler
+	{
+		@Override
+		public void onMouseOut(MouseOutEvent event) {
+			toolTip.hide();
+		}
 		
 	}
+	/**
+	 * To set Resource Grades
+	 * @param gradesText  {@value String}
+	 */
+	public void setGrades(String gradesText){
+
+		if(gradesText!=null&&!gradesText.equalsIgnoreCase("")&&!gradesText.equalsIgnoreCase("null")){
+			this.gradesText.setText(getGrades(gradesText));
+			gradesPanel.setVisible(true);
+		}else{
+			gradesPanel.setVisible(false);
+			//this.gradesText.setText(GL0977);
+		}
+	}
+	
+	public String getGrades(String grade){
+		if (grade != null) {
+			grade = grade.replace("null,", "").replace("null ,", "").replace("null", "");
+			
+		}
+		if (StringUtil.hasValidString(grade)) {
+			boolean isKindergarten = false;
+			boolean isHigherEducation = false;
+
+			if (grade.indexOf("Kindergarten") != -1) {
+				isKindergarten = true;
+			}
+
+			if (grade.indexOf("Higher Education") != -1) {
+				isHigherEducation = true;
+			}
+			if (grade.indexOf("-") > 0) {
+				if (grade.indexOf(",") == -1) {
+					grade = generateGradeIfHypen(grade);
+					
+				}
+			}
+
+			List<String> gradeList = Arrays.asList(grade.split(","));
+			int gradeListSize = gradeList.size();
+
+			StringBuilder finalGradeStringB = new StringBuilder();
+			
+			
+			List<Integer> gradeListInt = new ArrayList<Integer>();
+			//finalGradeStringB.append(gradeListSize > 1 ? "Grades: " : "Grade: ");
+			
+
+			
+			
+			for (String eachGrade1 : gradeList) {
+				if (!eachGrade1.equalsIgnoreCase("Kindergarten")
+						&& !eachGrade1.equalsIgnoreCase("Higher Education")) {
+
+					eachGrade1 = eachGrade1.replaceAll("th", "")
+							.replaceAll("TH", "").replaceAll("st", "")
+							.replaceAll("ST", "").replaceAll("nd", "")
+							.replaceAll("ND", "").replaceAll("rd", "")
+							.replaceAll("RD", "");
+					eachGrade1 = eachGrade1.toLowerCase()
+							.replaceAll("Grade", "").replaceAll("grade", "");
+					eachGrade1 = eachGrade1.toLowerCase().replaceAll("K-", "")
+							.replaceAll("k-", "");
+					eachGrade1 = eachGrade1.toLowerCase().replaceAll("K", "")
+							.replaceAll("k", "");
+					try {
+
+						
+						 
+					//	gradeListInt.clear();
+						String grad[] = generateGradeIfHypen(eachGrade1).trim().split(",");
+						for (int i = 0; i < grad.length; i++) {
+ 
+							gradeListInt.add(Integer.parseInt(grad[i]));
+							
+						}
+
+					} catch (Exception e) {
+						//gradeListInt.add(0);
+						e.printStackTrace();
+					}
+				}
+			}
+			gradeListInt = sortList(gradeListInt);
+			String finalGrde = formatGrades(gradeListInt);
+			if(finalGrde.equalsIgnoreCase(ALL_GRADES)){
+				finalGradeStringB.append(ALL_GRADES);
+			}else{
+			   if(isKindergarten&&isHigherEducation){
+				    finalGradeStringB.append(finalGrde.equalsIgnoreCase("")?"Kindergarten":"Kindergarten, ");
+					finalGradeStringB.append(finalGrde);
+					finalGradeStringB.append(finalGrde.equalsIgnoreCase("")?", Higher Education":", Higher Education");
+			   }else if(isKindergarten){
+					finalGradeStringB.append(finalGrde.equalsIgnoreCase("")?"Kindergarten":"Kindergarten, ");
+					finalGradeStringB.append(finalGrde);
+				}else if(isHigherEducation){
+					finalGradeStringB.append(finalGrde);
+					finalGradeStringB.append(finalGrde.equalsIgnoreCase("")?"Higher Education":", Higher Education");
+				}else{
+					finalGradeStringB.append(finalGrde);
+				}
+			}
+			
+
+			
+
+			grade = finalGradeStringB.toString();
+			
+		}
+		return grade;
+	}
+	private String generateGradeIfHypen(String grade) {
+		String gradeList[];
+	 
+		StringBuilder gradeStr = new StringBuilder();
+		gradeList = grade.split("-");
+		if (gradeList.length >= 2) {
+			int start = Integer.parseInt(gradeList[0].trim());
+			int end = Integer.parseInt(gradeList[1].trim());
+			if (start < end) {
+				for (int i = start; i <= end; i++) {
+					if (i == end) {
+						gradeStr.append(i);
+					} else {
+						gradeStr.append(i).append(",");
+					}
+				}
+			}
+		} else {
+			gradeStr.append(Math.round(Double.parseDouble(gradeList[0].trim())));
+			/*gradeStr.append(Integer.parseInt("0"));*/
+		}
+		return gradeStr.toString();
+	}	
+	public List<Integer> sortList(List<Integer> list) {
+
+		int listSize = list.size();
+
+		for (int i = 0; i < listSize; i++) {
+
+			for (int j = 1; j < listSize - i; j++) {
+				if (list.get(j - 1) > list.get(j)) {
+					int temp = list.get(j - 1);
+					list.set(j - 1, list.get(j));
+					list.set(j, temp);
+				}
+			}
+		}
+
+		return list;
+	}
+	private String formatGrades(List<Integer> list) {
+		
+    	StringBuffer grade = new StringBuffer();
+		try {
+			if(list.size()>3){
+                int firstGrade=list.get(0);
+                int lastGrade=list.get(list.size()-1);
+                String displayGrade=firstGrade+"-"+lastGrade;
+                if(list.size()>=5){
+                	if(firstGrade<=4&&lastGrade>=9){
+                		grade.append("ALL GRADES");
+                	}
+                	else{
+                		grade.append(firstGrade);
+                		grade.append("-");
+                		grade.append(lastGrade);
+                	}
+                	
+                }else{
+                        if(displayGrade.equalsIgnoreCase("1-12")){
+                                grade.append("ALL GRADES");
+                        }else{
+                                grade.append(firstGrade);
+                                grade.append("-");
+                                grade.append(lastGrade);
+                        }
+                }
+        }else{
+                for(int i=0;i<list.size();i++){
+                	grade.append(list.get(i));
+                	if(i!=(list.size()-1)){
+						grade.append(", ");
+					}
+                }
+        }
+			
+			
+		} catch (Exception e) {
+		}
+		return grade.toString();
+	}
+
 
 }
