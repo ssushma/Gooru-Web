4a3d63aaa6bea8fa03745da6ca87e29fd44d8ee6
ssushma <ssushma@goorulearning.org>
2014-05-21 20:58:54 +0530

    Add Tags spell error corrected

--- src/main/java/org/ednovo/gooru/client/mvp/shelf/collection/tab/resource/item/ShelfCollectionResourceChildView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/shelf/collection/tab/resource/item/ShelfCollectionResourceChildView.java
@@ -340,7 +340,7 @@ public class ShelfCollectionResourceChildView extends
 		StartPageLbl.setText(GL0961);
 		EditBtn.setText(GL0140);
 		updateResourceBtn.setText(GL0962);
-		addTages.setText("Add Tages");
+		addTages.setText("Add Tags");
 		editInfoLbl.setText(GL0963);
 		editVideoTimeLbl.setText(GL0964);
 		editStartPageLbl.setText(GL0960);
