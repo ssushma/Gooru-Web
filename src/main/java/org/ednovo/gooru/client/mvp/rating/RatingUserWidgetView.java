e54e28e48fe999de22579d2aa9f8106fee3398ea
vinay <vinay.r@goorulearning.org>
2014-05-26 18:51:48 +0530

    Implemented create date for reviews.

--- src/main/java/org/ednovo/gooru/client/mvp/rating/RatingUserWidgetView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/rating/RatingUserWidgetView.java
@@ -1,11 +1,15 @@
 package org.ednovo.gooru.client.mvp.rating;
 
+import java.util.Date;
+
 import org.ednovo.gooru.client.gin.AppClientFactory;
 import org.ednovo.gooru.client.uc.PlayerBundle;
 import org.ednovo.gooru.shared.model.content.StarRatingsDo;
+import org.ednovo.gooru.shared.util.MessageProperties;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -17,7 +21,7 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Widget;
 
-public class RatingUserWidgetView extends Composite {
+public class RatingUserWidgetView extends Composite implements MessageProperties {
 
 	@UiField HTMLPanel reviewContainer;
 	
@@ -35,6 +39,8 @@ public class RatingUserWidgetView extends Composite {
 	
 	@UiField RatingAndReviewStyleBundle style;
 	
+	private static final String DATE_FORMAT="MMMM dd, yyyy";
+	
 	private static RatingUserWidgetViewUiBinder uiBinder = GWT
 			.create(RatingUserWidgetViewUiBinder.class);
 
@@ -48,7 +54,8 @@ public class RatingUserWidgetView extends Composite {
 	}
 	
 	public void setData(StarRatingsDo starRatingsDo) {
-		//timeStamp.setText("3 days ago");
+		String commentTime = getCreatedTime(Long.toString(starRatingsDo.getCreatedDate())); 
+		timeStamp.setText(commentTime);
 		review.setText(starRatingsDo.getFreeText());
 		editReviewText.setText(starRatingsDo.getFreeText());
 		editReview.setVisible(false);
@@ -178,5 +185,36 @@ public class RatingUserWidgetView extends Composite {
 		cancelReviewBtn.removeStyleName(style.editReview());
 	}
 	
+	private String getCreatedTime(String commentCreatedTime) {
+		String createdTime = null;
+		Long currentTime = System.currentTimeMillis();
+		Long commentTime = Long.parseLong(commentCreatedTime);
+		Long elapsedTime = (currentTime - commentTime);
+		int seconds = (int) (elapsedTime / 1000) % 60 ;
+		int minutes = (int) ((elapsedTime / (1000*60)) % 60);
+		int hours   = (int) ((elapsedTime / (1000*60*60)) % 24);
+		int days = (int) (elapsedTime / (1000*60*60*24));
+		Date currentDate = new Date(commentTime);
+		DateTimeFormat fmt = DateTimeFormat.getFormat (DATE_FORMAT);
+		if(days>6){
+			createdTime = fmt.format (currentDate);
+		}
+		else if(days>0&&days<=6) {
+			createdTime = days + getTimePrefix(days," "+GL0562, GL0579, GL0580);
+		} else if(hours>0&&hours<24) {
+			createdTime = hours + getTimePrefix(hours," "+GL0563, GL1435, GL1436);
+		} else if(minutes>0&&minutes<60) {
+			createdTime = minutes + getTimePrefix(minutes," "+GL0564, GL1437, GL1438);
+		} else if(seconds<=60) {
+			createdTime = GL0561;
+		}
+		return createdTime;
+	}
 	
+	private String getTimePrefix(int count, String msg, String regex, String replacement) {
+		if(count==1) {
+			msg = msg.replaceAll(regex, replacement);
+		}
+		return msg;
+	}
 }
