f1335fcedefe31bd31bd7bda5436b75609b201c3
vinay <vinay.r@goorulearning.org>
2014-06-18 13:57:27 +0530

    Implemented re-order functionality on changind dots in a class page.

--- src/main/java/org/ednovo/gooru/client/mvp/classpages/edit/EditClasspagePresenter.java
+++ src/main/java/org/ednovo/gooru/client/mvp/classpages/edit/EditClasspagePresenter.java
@@ -188,7 +188,6 @@ public class EditClasspagePresenter extends BasePlacePresenter<IsEditClasspageVi
 	@Override
 	public void onBind() {
 		super.onBind();
-		
 		setCollectionAsyncCallback(new SimpleAsyncCallback<CollectionDo>() {
 
 			@Override
@@ -563,6 +562,7 @@ public class EditClasspagePresenter extends BasePlacePresenter<IsEditClasspageVi
 			public void onSuccess(ArrayList<ClasspageItemDo> classpageItemsList) {
 				if(classpageItemsList!=null){
 					getView().displayAssignmentPath(classpageItemsList);
+					getClasspage();
 				}
 			}
 		});
