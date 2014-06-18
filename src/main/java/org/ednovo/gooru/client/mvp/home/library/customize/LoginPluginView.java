56d57cbb090d7d42ddb716647b7fbb0d080c029e
vinay <vinay.r@goorulearning.org>
2014-06-18 19:12:33 +0530

    Added alert pop-up to check blocked user while logging in through gmail.

--- src/main/java/org/ednovo/gooru/client/mvp/home/library/customize/LoginPluginView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/home/library/customize/LoginPluginView.java
@@ -345,13 +345,27 @@ public abstract class LoginPluginView extends ChildView<LoginPluginPresenter> im
 	public void onGmailButtonClicked(ClickEvent clickEvent){
 		DataLogEvents.signIn(GwtUUIDGenerator.uuid(),"login",System.currentTimeMillis(),System.currentTimeMillis(), "", AppClientFactory.getLoggedInUser().getToken());
 		String callBack = Window.Location.getHref();
-	
+		/**
+		 * Added to check for blocked user its a temporary code
+		 */
+		final String loginType = AppClientFactory.getLoggedInUser().getLoginType() !=null ? AppClientFactory.getLoggedInUser().getLoginType() : "";
+		final UserDo userDo = AppClientFactory.getLoggedInUser(); 
+//		----------------------------------------------------------------- //
 		AppClientFactory.getInjector().getSearchService().getGoogleSignin(callBack, new SimpleAsyncCallback<String>() {
 		
 			@Override
 			public void onSuccess(String result) {
 //				MixpanelUtil.Click_Gmail_SignIn("LoginPopup");
 				Window.Location.replace(result);
+				/**
+				 * Added to check for blocked user its a temporary code
+				 */
+				if(!AppClientFactory.isAnonymous() && loginType.equalsIgnoreCase("apps")) {
+					if(userDo.getActive()==1){
+						new AlertContentUc("To check ", "checking google sign in");
+					}
+				}
+//				----------------------------------------------------------------- //
 			
 			}
 		});
