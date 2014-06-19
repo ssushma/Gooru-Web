edf9533aa239e92891692bc6f2076c1fff53abd7
Surabhi Seetharam <sseetha2@ncsu.edu>
2014-06-19 10:03:08 -0700

    Text Changes based on Media Scripts

--- src/main/java/org/ednovo/gooru/shared/util/MessageProperties.java
+++ src/main/java/org/ednovo/gooru/shared/util/MessageProperties.java
@@ -749,7 +749,7 @@ public interface MessageProperties {
 	//Add collection view messages.
 	
 	String GL0589 =	"View the collection in the My Collections tab";
-	String GL0590 =	"Add";
+	String GL0590 =	"Save to Customize";
 	String GL0591 ="Adding....";
 	String GL0592 ="Hide";
 	String GL0593 ="Rename (optional)";
