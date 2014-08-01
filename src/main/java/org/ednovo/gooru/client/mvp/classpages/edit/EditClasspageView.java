4d1b54c7ef4dd0b045c58db5cba8dde0543e0427
Sushma <ssushma@goorulearning.org>
2014-08-01 16:27:34 +0530

    Classpage sort options issue resolved on Assignments tab click.

--- src/main/java/org/ednovo/gooru/client/mvp/classpages/edit/EditClasspageView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/classpages/edit/EditClasspageView.java
@@ -343,6 +343,7 @@ public class EditClasspageView extends
 		panelProgressContainer.setVisible(true);
 		noAssignmentsMessagePanel.setVisible(false);
 		assignmentsTabContainerPanel.setVisible(true);
+		assignmentsTab.setEnabled(false);
 		btnCollectionEditImage.setVisible(false);
 		
 		/*lblInstructionalPlaceHolder.setText(GL1946);*/
@@ -423,6 +424,7 @@ public class EditClasspageView extends
 			
 			newAssignmentAndMsgPanel.setVisible(true);
 			assignmentsTabContainerPanel.setVisible(true);
+			assignmentsTab.setEnabled(false);
 			getClassListContainer().setVisible(false);
 
 		
@@ -1049,6 +1051,7 @@ public class EditClasspageView extends
 			monitorProgress.setVisible(false);
 			newAssignmentAndMsgPanel.setVisible(false);
 			assignmentsTabContainerPanel.setVisible(false);
+			assignmentsTab.setEnabled(true);
 			getClassListContainer().setVisible(true);
 			frameDiv.setVisible(false);
 			
@@ -1061,6 +1064,7 @@ public class EditClasspageView extends
 			classListTab.getElement().setClassName("");
 			newAssignmentAndMsgPanel.setVisible(false);
 			assignmentsTabContainerPanel.setVisible(false);
+			assignmentsTab.setEnabled(true);
 			getClassListContainer().setVisible(false);
 			assignmentsDirectionsLabel.setVisible(false);
 			getstarteddiv.removeStyleName(EditClasspageCBundle.INSTANCE.css().btnContainerClasswithBG());
@@ -1609,6 +1613,7 @@ public class EditClasspageView extends
 		
 		newAssignmentAndMsgPanel.setVisible(true);
 		assignmentsTabContainerPanel.setVisible(true);
+		assignmentsTab.setEnabled(false);
 		getClassListContainer().setVisible(false);
 	}
 /**
@@ -1788,6 +1793,7 @@ public class EditClasspageView extends
 			
 			newAssignmentAndMsgPanel.setVisible(true);
 			assignmentsTabContainerPanel.setVisible(true);
+			assignmentsTab.setEnabled(false);
 			getClassListContainer().setVisible(false);
 			if(tab!=null){
 				Map<String,String> params = new HashMap<String,String>();
@@ -1821,6 +1827,7 @@ public class EditClasspageView extends
 			dropDownListDiv.setVisible(false);
 			newAssignmentAndMsgPanel.setVisible(false);
 			assignmentsTabContainerPanel.setVisible(false);
+			assignmentsTab.setEnabled(true);
 			panelAssignmentPath.setVisible(false);
 			headerAssignments.setVisible(false);
 			panelProgressContainer.setVisible(false);
@@ -1863,6 +1870,7 @@ public class EditClasspageView extends
 			backArrowButton.setVisible(false);
 			monitorProgress.setVisible(false);
 			assignmentsTabContainerPanel.setVisible(false);
+			assignmentsTab.setEnabled(true);
 			getClassListContainer().setVisible(false);
 			panelAssignmentPath.setVisible(false);
 			headerAssignments.setVisible(false);
