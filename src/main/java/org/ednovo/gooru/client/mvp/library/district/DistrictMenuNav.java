0bd4cf1c4d8e00a68a35d371efc668fe23055d4d
T Anil Kumar <anil@goorulearning.org>
2014-08-10 12:47:11 +0530

    Fixes.

--- src/main/java/org/ednovo/gooru/client/mvp/library/district/DistrictMenuNav.java
+++ src/main/java/org/ednovo/gooru/client/mvp/library/district/DistrictMenuNav.java
@@ -148,7 +148,8 @@ public abstract class DistrictMenuNav extends Composite {
 
 		if(AppClientFactory.getCurrentPlaceToken().equals(PlaceTokens.LIFEBOARD)
 					||AppClientFactory.getCurrentPlaceToken().equals(PlaceTokens.VALVERDE)
-					||AppClientFactory.getCurrentPlaceToken().equals(PlaceTokens.SUSD)) {
+					||AppClientFactory.getCurrentPlaceToken().equals(PlaceTokens.SUSD)
+					||AppClientFactory.getCurrentPlaceToken().equals(PlaceTokens.RUSD_LIBRARY)) {
 			
 			learnPanel.addStyleName(districtStyleUc.tabsLiInactive());
 		} else {
