ce0c4a19af9bc29a47f3156e55eeb29e6322cd49
vinay <vinay.r@goorulearning.org>
2014-09-17 15:26:07 +0530

    Added loading Icons for path way.

--- src/main/java/org/ednovo/gooru/client/mvp/classpages/unitSetup/UnitsAssignmentWidgetView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/classpages/unitSetup/UnitsAssignmentWidgetView.java
@@ -75,7 +75,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 	}
 	
 	
-	@UiField HTMLPanel assignmentsContainer;
+	@UiField HTMLPanel assignmentsContainer,loadingImageLabel;
 	
 	@UiField Button editUnitButton,addAssignmentButton,cancelEditButton;
 	
@@ -113,6 +113,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 		initWidget(uibinder.createAndBindUi(this));
 		this.classUnitsDo=classUnitsDo;
 		addAssignmentButton.getElement().getStyle().setMarginTop(28, Unit.PX);
+		loadingImageLabel.setVisible(false);
 		setAssignmentsForUnit();
 		setUnitNameDetails();
 		cancelEditButton.setVisible(false);
@@ -135,6 +136,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 
 
 	private void setAssignmentsForUnit() {
+		loadingImageLabel.setVisible(false);
 		assignmentsContainer.clear();
 		if(classUnitsDo!=null && classUnitsDo.getResource()!=null){
 			if(classUnitsDo.getResource().getCollectionItems().size()==0){
@@ -193,6 +195,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 								if(isAssignmentDeleted){
 									clearAssignmentsFromDo();
 									hide();
+									loadingImageLabel.setVisible(true);
 									if((getTotalHitCount()-1)==assignmentOffset){
 										assignmentOffset=assignmentOffset-10;
 									}
@@ -210,6 +213,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 	
 	
 	public void setAssignmentsEditView() {
+		loadingImageLabel.setVisible(false);
 		assignmentsContainer.clear();
 		for(int i=0;i<classUnitsDo.getResource().getCollectionItems().size();i++){
 			AssignmentEditView assignmentEditView = new AssignmentEditView(classUnitsDo);
@@ -374,6 +378,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 	@UiHandler("htPanelNextArrow")
 	public void clickOnNextArrow(ClickEvent clickEvent){
 		clearAssignmentsFromDo();
+		loadingImageLabel.setVisible(true);
 		getUnitAssignments(getAssignmentOffsetValue(NEXT),isEditMode);
 	}
 	
@@ -390,6 +395,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 	@UiHandler("htPanelPreviousArrow")
 	public void clickOnPreviousArrow(ClickEvent clickEvent){
 		clearAssignmentsFromDo();
+		loadingImageLabel.setVisible(true);
 		getUnitAssignments(getAssignmentOffsetValue(PREVIOUS),isEditMode);
 	}
 	
@@ -402,8 +408,8 @@ public class UnitsAssignmentWidgetView extends Composite {
 				setTotalHitCount(result.getTotalHitCount());
 				classUnitsDo.getResource().setCollectionItems(result.getSearchResults());
 				if(isAssignmentEditmode){
-					setAssignmentsEditView();
 					classUnitsDo.getResource().setItemCount(getTotalHitCount());
+					setAssignmentsEditView();
 				}else{
 					setAssignmentsForUnit();
 				}
@@ -415,6 +421,7 @@ public class UnitsAssignmentWidgetView extends Composite {
 	
 	public void addAssignment(ArrayList<ClasspageItemDo> classpageItemDo){
 		setTotalHitCount(getTotalHitCount()+classpageItemDo.size());
+		loadingImageLabel.setVisible(true);
 		getUnitAssignments(getOffsetValue(),false);
 	}
 	
