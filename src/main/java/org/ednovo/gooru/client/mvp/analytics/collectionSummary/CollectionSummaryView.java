1359be4d0520cb24a774848175e9cbf92b2ab428
Sushma <ssushma@goorulearning.org>
2014-09-19 16:44:43 +0530

    aanalytics issues resolved

--- src/main/java/org/ednovo/gooru/client/mvp/analytics/collectionSummary/CollectionSummaryView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/analytics/collectionSummary/CollectionSummaryView.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.ednovo.gooru.client.gin.BaseViewWithHandlers;
-import org.ednovo.gooru.client.mvp.analytics.util.AnalyticsUtil;
+import org.ednovo.gooru.client.mvp.Analytics.util.AnalyticsUtil;
 import org.ednovo.gooru.shared.model.analytics.CollectionSummaryMetaDataDo;
 import org.ednovo.gooru.shared.model.analytics.CollectionSummaryUsersDataDo;
 import org.ednovo.gooru.shared.model.analytics.UserDataDo;
