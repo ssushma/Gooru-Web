83d9c5ba3ade0ef793f441ab929f32a98bd47086
janamitra <janamithra@goorulearning.org>
2014-10-01 16:03:28 +0530

    "Unit goals update event handled and validations added."

--- src/main/java/org/ednovo/gooru/client/mvp/classpages/unitdetails/UnitAssignmentPresenter.java
+++ src/main/java/org/ednovo/gooru/client/mvp/classpages/unitdetails/UnitAssignmentPresenter.java
@@ -26,6 +26,7 @@ package org.ednovo.gooru.client.mvp.classpages.unitdetails;
 import org.ednovo.gooru.client.PlaceTokens;
 import org.ednovo.gooru.client.SimpleAsyncCallback;
 import org.ednovo.gooru.client.gin.AppClientFactory;
+import org.ednovo.gooru.client.mvp.classpages.event.UpdateUnitSetGoalEvent;
 import org.ednovo.gooru.client.mvp.classpages.unitdetails.personalize.PersonalizeUnitPresenter;
 import org.ednovo.gooru.shared.model.content.ClassDo;
 import org.ednovo.gooru.shared.model.content.ClassUnitsListDo;
@@ -192,7 +193,8 @@ public class UnitAssignmentPresenter extends PresenterWidget<IsUnitAssignmentVie
 			@Override
 			public void onSuccess(ClassUnitsListDo result) {
 				// TODO Auto-generated method stub
-				System.out.println("mini::::::"+result.getMinimumScoreByUser());
+				String updatedTime=(result.getTimeStudying()!=null && !result.getTimeStudying().equals("")) ? result.getTimeStudying():null;
+				AppClientFactory.fireEvent(new UpdateUnitSetGoalEvent(0, 0,updatedTime));
 				
 			}
 		});
