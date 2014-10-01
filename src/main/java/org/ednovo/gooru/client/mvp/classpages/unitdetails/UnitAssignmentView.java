7ac240b15bd506a9c573ce3f776c6da80b6fa376
vinay <vinay.r@goorulearning.org>
2014-10-01 16:58:31 +0530

    Issue rectified with right and left arrow while reorder in unit details page.

--- src/main/java/org/ednovo/gooru/client/mvp/classpages/unitdetails/UnitAssignmentView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/classpages/unitdetails/UnitAssignmentView.java
@@ -240,6 +240,7 @@ public class UnitAssignmentView extends BaseViewWithHandlers<UnitAssignmentUiHan
 			resetCircleAndAssignmentContainer(unitTitle);
 			setClassUnitsListDo(unitsWidget.getClassUnitDo());
 			revealPlace("unitdetails",null,unitsWidget.getUnitGooruOid(),null);
+			assignmentOffset=0;
 			scoreHederView(unitsWidget.getClassUnitDo());
 			removeAndAddUnitSelectedStyle();
 		}
@@ -699,6 +700,7 @@ public class UnitAssignmentView extends BaseViewWithHandlers<UnitAssignmentUiHan
 		if(classPageId==null){
 			classPageId= AppClientFactory.getPlaceManager().getRequestParameter("id", null);
 		}
+		unitId = AppClientFactory.getPlaceManager().getRequestParameter("uid",null);
 		AppClientFactory.getInjector().getClasspageService().v2GetPathwayItems(classPageId, unitId, "sequence", assignmentLimit, assignmentOffset, new SimpleAsyncCallback<UnitAssignmentsDo>() {
 
 			@Override
@@ -737,10 +739,11 @@ public class UnitAssignmentView extends BaseViewWithHandlers<UnitAssignmentUiHan
 			leftArrow.setUrl("images/leftSmallarrow.png");
 			leftArrow.getElement().setAttribute("style","margin-left: 10px");
 			circleContainerPanel.add(leftArrow);
-
+			if(unitAssignmentsDo.getSearchResults().size()>0&&aid==null){
+				aid=unitAssignmentsDo.getSearchResults().get(0).getCollectionItemId();
+			}
 			for(int i=0;i<unitAssignmentsDo.getSearchResults().size();i++){
 				unitCricleViewObj =new UnitCricleView(unitAssignmentsDo.getSearchResults().get(i));
-				
 				//unitCricleViewObj.getElement().setId(unitAssignmentsDo.getSearchResults().get(i).getResource().getGooruOid()+"");
 				unitCricleViewObj.getElement().setId(unitAssignmentsDo.getSearchResults().get(i).getCollectionItemId());
 				circleContainerPanel.add(unitCricleViewObj);
@@ -1014,7 +1017,7 @@ public class UnitAssignmentView extends BaseViewWithHandlers<UnitAssignmentUiHan
 	}
 	
 	public void setAssignmentToNewPosition(int selectedAssignmentpageNumber,String selectedPathId,int totalHintCount,String selectedAssignmentId,UnitCricleView UnitCricleObj,int mouseOverAssignmentSeqPos){
-				
+		unitId = AppClientFactory.getPlaceManager().getRequestParameter("uid",null);
 		if(selectedPathId.equalsIgnoreCase(unitId)){
 			assignmentOffset =(selectedAssignmentpageNumber/assignmentLimit)*assignmentLimit;
 			if(assignmentOffset==selectedAssignmentpageNumber){
