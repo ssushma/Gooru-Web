d1e83b5414c9a54ef0eebce07949c68081a4220c
shiva <shivalinga@goorulearning.org>
2014-10-16 14:18:25 +0530

    new html tags.

---
+++ src/main/java/org/ednovo/gooru/client/htmltags/SectionTag.java
@@ -0,0 +1,20 @@
+package org.ednovo.gooru.client.htmltags;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.ui.ComplexPanel;
+import com.google.gwt.user.client.ui.InsertPanel;
+import com.google.gwt.user.client.ui.Widget;
+public class SectionTag extends ComplexPanel implements InsertPanel{
+	public SectionTag() {
+		setElement(Document.get().createElement("section"));
+	}
+
+	@Override
+	public void add(Widget w){
+		add(w,getElement());
+	}
+	
+	public void insert(Widget w,int beforeIndex){
+		insert(w, getElement(), beforeIndex, true);
+	}
+}
