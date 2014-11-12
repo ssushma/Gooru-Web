c6700fd5aa26f5edb36e69955458d53b214bdd00
Sravan <sravan.reddy@ideabrahma.com>
2014-11-12 17:06:39 +0530

    Search NoResult Page is Responsive

--- src/main/java/org/ednovo/gooru/client/mvp/search/collection/CollectionSearchResultVc.java
+++ src/main/java/org/ednovo/gooru/client/mvp/search/collection/CollectionSearchResultVc.java
@@ -165,7 +165,7 @@ public class CollectionSearchResultVc extends Composite implements IsDraggable, 
 		imgOER.getElement().setAttribute("alt", i18n.GL1834());
 		imgOER.getElement().setAttribute("title", i18n.GL1834());
 		
-		collectionDescriptionHtml.getElement().getStyle().setTextAlign(TextAlign.LEFT);
+		collectionDescriptionHtml.getElement().getStyle().setTextAlign(TextAlign.JUSTIFY);
 		
 		
 		AppClientFactory.getEventBus().addHandler(UpdateSearchResultMetaDataEvent.TYPE,setUpdateMetaData);
