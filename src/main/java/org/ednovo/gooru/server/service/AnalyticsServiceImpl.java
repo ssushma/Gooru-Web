3a7fd8a63ec90feeb3fbd82e39f152e4723019e4
suryanarayana <suryanarayana@ideabrahma.com>
2014-11-28 13:04:57 +0530

    "changed the playloadobject to payloadobject"

--- src/main/java/org/ednovo/gooru/server/service/AnalyticsServiceImpl.java
+++ src/main/java/org/ednovo/gooru/server/service/AnalyticsServiceImpl.java
@@ -420,7 +420,7 @@ public class AnalyticsServiceImpl extends BaseServiceImpl implements AnalyticsSe
 			mainObj.put("parentItemId",parentItemId);
 			mainObj.put("parentGooruOId",collectionId);
 			mainObj.put("freeText",freeText);
-			mainObj.put("playLoadObject",setPlayLoadObj.toString());
+			mainObj.put("payLoadObject",setPlayLoadObj.toString());
 			mainObj.put("user",userObj);
 			System.out.println("mainObj.toString()::"+mainObj.toString());
 			JsonResponseRepresentation jsonResponseRep = ServiceProcessor.post(url, getRestUsername(), getRestPassword(),mainObj.toString());
