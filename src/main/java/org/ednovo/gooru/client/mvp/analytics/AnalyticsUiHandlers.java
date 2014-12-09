9facfb3f286a739780b7c25cb90fb61153a582b1
suryanarayana <suryanarayana@ideabrahma.com>
2014-12-09 11:27:47 +0530

    "Added gwt analytics files"

---
+++ src/main/java/org/ednovo/gooru/client/mvp/analytics/AnalyticsUiHandlers.java
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ * Copyright 2013 Ednovo d/b/a Gooru. All rights reserved.
+ * 
+ *  http://www.goorulearning.org/
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+package org.ednovo.gooru.client.mvp.analytics;
+
+import org.ednovo.gooru.client.gin.BaseUiHandlers;
+import org.ednovo.gooru.shared.model.content.UnitAssignmentsDo;
+
+public interface AnalyticsUiHandlers extends BaseUiHandlers{
+	
+	/**
+	 * This method is used to get the pathway items.
+	 * @param classpageId
+	 * @param pathwayGooruOid
+	 * @param sequence
+	 * @param limit
+	 * @param offSet
+	 */
+	void getPathwayItems(String classpageId,String pathwayGooruOid,String sequence,int limit,int offSet);
+	
+	/**
+	 * This method is used to get unit assignments items.
+	 * @param classpageId
+	 * @param pathwayGooruOid
+	 * @param sequence
+	 * @param limit
+	 * @param offSet
+	 */
+	void getUnitAssignments(final String classpageId, final String pathwayGooruOid,String sequence,int limit,int offSet);
+	
+	/**
+	 * This method is used to get the pathway units
+	 * @param classId
+	 * @param limit
+	 * @param offset
+	 * @param clearPanel
+	 */
+	void getPathwayUnits(String classId,int limit, int offset ,boolean clearPanel);
+	
+	/**
+	 * This method is used to set the clicked tab presenter on the slot.
+	 * @param clickedTab
+	 * @param collectionId
+	 * @param selectedCollectionTitle
+	 */
+	void setClickedTabPresenter(String clickedTab,String collectionId,String selectedCollectionTitle);
+	
+	/**
+	 * This method is used to get the bottom scored students data.
+	 * @param classpageId
+	 * @param pathwayId
+	 * @param collectionId
+	 * @param sortOrder
+	 */
+	void getBottomStudentsData(String classpageId, String pathwayId,String collectionId,String sortOrder);
+	
+	/**
+	 * This method is used to get the top scored students data
+	 * @param classpageId
+	 * @param pathwayId
+	 * @param collectionId
+	 * @param sortOrder
+	 */
+	void getTopStudentsData(String classpageId, String pathwayId,String collectionId,String sortOrder);
+	
+	/**
+	 * This method is used to get the collections.
+	 * @param classpageId
+	 * @param pathwayId
+	 */
+	void getGradeCollectionJson(String classpageId,String pathwayId);
+	
+	/**
+	 * This method is used to export the OE responses.
+	 * @param classpageId
+	 * @param pathwayId
+	 * @param timeZone
+	 */
+	void exportOEPathway(String classpageId,String pathwayId,String timeZone);
+	
+	/**
+	 * This method is used to get the unit Assignments.
+	 */
+	void getUnitAssignments();
+	
+	/**
+	 * This method is used to set the clicked unit assignment details in the slot.
+	 * @param result
+	 * @param classpageId
+	 * @param pathwayId
+	 */
+	void setAnalyticsAssignmentsPresenter(UnitAssignmentsDo result,String classpageId,String pathwayId);
+	
+	/**
+	 * This method is used to set the personalize data.
+	 */
+	void setPersonalizeData();
+}
---
+++ src/main/java/org/ednovo/gooru/client/mvp/analytics/collectionSummaryIndividual/CollectionOverViewWidget.java
@@ -0,0 +1,71 @@
+package org.ednovo.gooru.client.mvp.analytics.collectionSummaryIndividual;
+
+import org.ednovo.gooru.client.mvp.analytics.util.AnalyticsReactionWidget;
+import org.ednovo.gooru.client.mvp.analytics.util.AnalyticsUtil;
+import org.ednovo.gooru.shared.i18n.MessageProperties;
+import org.ednovo.gooru.shared.model.analytics.CollectionSummaryMetaDataDo;
+import org.ednovo.gooru.shared.util.StringUtil;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.TextAlign;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+public class CollectionOverViewWidget extends Composite {
+
+	private static CollectionOverViewWidgetUiBinder uiBinder = GWT
+			.create(CollectionOverViewWidgetUiBinder.class);
+
+	interface CollectionOverViewWidgetUiBinder extends
+			UiBinder<Widget, CollectionOverViewWidget> {
+	}
+	private static MessageProperties i18n = GWT.create(MessageProperties.class);
+	
+	@UiField Label lblViews,lblAvgReaction,collectionOverViewlbl,timeSpentTxtlbl,totalTimeSpentlbl,totalViewlbl;
+	@UiField HTMLPanel totalAvgReactionlbl;
+	
+	/**
+	 * Constructor
+	 */
+	public CollectionOverViewWidget() {
+		initWidget(uiBinder.createAndBindUi(this));
+		setStaticData();
+	}
+	/**
+	 * This method is used to set static data
+	 */
+	void setStaticData(){
+		
+		StringUtil.setAttributes(totalAvgReactionlbl.getElement(), "pnlTotalAvgReactionlbl", null, null);
+		
+		StringUtil.setAttributes(collectionOverViewlbl.getElement(), "lblCollectionOverView", i18n.GL2274(), i18n.GL2274());
+		StringUtil.setAttributes(timeSpentTxtlbl.getElement(), "lblTimeSpentTxt", null, null);
+		StringUtil.setAttributes(totalTimeSpentlbl.getElement(), "lblTotalTimeSpent", null, null);
+		StringUtil.setAttributes(totalViewlbl.getElement(), "lblTotalViewlbl", null, null);
+		StringUtil.setAttributes(lblViews.getElement(), "lblViews", i18n.GL2276(), i18n.GL2276());
+		StringUtil.setAttributes(lblAvgReaction.getElement(), "lblAvgReaction", i18n.GL2277(), i18n.GL2277());
+		
+	}
+	/**
+	 * This method is used to set the data.
+	 * @param result
+	 * @param isTeacherSummary
+	 */
+	public void setData(CollectionSummaryMetaDataDo result,boolean isTeacherSummary){
+		collectionOverViewlbl.getElement().getStyle().setTextAlign(TextAlign.LEFT);
+		if(isTeacherSummary){
+			timeSpentTxtlbl.setText(i18n.GL2230());
+		}else{
+			timeSpentTxtlbl.setText(i18n.GL2275());
+		}
+		totalTimeSpentlbl.setText(AnalyticsUtil.getTimeSpent(result.getAvgTimeSpent()));
+		totalViewlbl.setText(Integer.toString(result.getViews()));
+		totalAvgReactionlbl.clear();
+		totalAvgReactionlbl.add(new AnalyticsReactionWidget(result.getAvgReaction()));
+	}
+
+}
