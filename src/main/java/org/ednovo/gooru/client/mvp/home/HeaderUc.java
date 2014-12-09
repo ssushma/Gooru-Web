29790b0235846ffc80eb05723f1f7636dd77eda3
Sushma <ssushma@goorulearning.org>
2014-12-09 19:12:23 +0530

    Search collapsible filters design implemented

--- src/main/java/org/ednovo/gooru/client/mvp/home/HeaderUc.java
+++ src/main/java/org/ednovo/gooru/client/mvp/home/HeaderUc.java
@@ -1424,7 +1424,7 @@ public class HeaderUc extends Composite implements
 		}
 		params.put("category", "All");
 		params.put("query", getEditSearchText());
-		params.put(IsSearchView.RATINGS_FLT, "5,4,3,2,1");
+		params.put(IsSearchView.RATINGS_FLT, "5,4,3,2,1,0");
 		params.put("pageNum", "1");
 		params.put("pageSize", "8");
 		
