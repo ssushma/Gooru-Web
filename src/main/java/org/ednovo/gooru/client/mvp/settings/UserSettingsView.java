fc93da96be266cdc5e41b2dcdd14c448928e2743
T Anil Kumar <anil@goorulearning.org>
2014-12-10 12:13:28 +0530

    DO-6095 : 1.0.e [System - Setting] Super admin role should see "View Admin Portal" button at Setting - Implemented.

--- src/main/java/org/ednovo/gooru/client/mvp/settings/UserSettingsView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/settings/UserSettingsView.java
@@ -741,12 +741,14 @@ public class UserSettingsView extends BaseViewWithHandlers<UserSettingsUiHandler
 		btnViewAdmin.getElement().setId("btnBtnViewAdmin");
 		btnViewAdmin.getElement().setAttribute("alt", i18n.GL1993() );
 		btnViewAdmin.getElement().setAttribute("title", i18n.GL1993() );
-//		if(AppClientFactory.getLoggedInUser().getUserRoleSetString().contains("Content_Admin")){
-//			btnViewAdmin.setVisible(true);
-//		}else{
-//			btnViewAdmin.setVisible(false);
-//			
-//		}
+
+		String userRoles = AppClientFactory.getLoggedInUser().getUserRoleSetString();
+		if(userRoles.contains("Content_Admin") || userRoles.contains("superadmin")){
+			btnViewAdmin.setVisible(true);
+		}else{
+			btnViewAdmin.setVisible(false);
+			
+		}
 
 		
 		standardSavingTextLabel.setText("");
