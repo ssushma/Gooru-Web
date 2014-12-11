1c136a0b0fc5479e506f0ed9fa1e5ab2e9b5a095
janamitra <janamithra@goorulearning.org>
2014-12-11 14:05:21 +0530

    Modified filter values based on Api request parameters

--- src/main/java/org/ednovo/gooru/client/mvp/home/PreFilterPopup.java
+++ src/main/java/org/ednovo/gooru/client/mvp/home/PreFilterPopup.java
@@ -90,7 +90,7 @@ public class PreFilterPopup extends PopupPanel {
 	
 	String[] elementaryGrades = new String[]{i18n.GL3070(),i18n.GL3071(),i18n.GL3072(),i18n.GL3073(),i18n.GL3074()};
 	String[] middleGrades = new String[]{i18n.GL3075(),i18n.GL3076(),i18n.GL3077(),i18n.GL3078(),i18n.GL3079()};
-	String[] higherGrades = new String[]{i18n.GL3080(),i18n.GL3081(),i18n.GL3082(),i18n.GL3083(),"13+"};
+	String[] higherGrades = new String[]{i18n.GL3080(),i18n.GL3081(),i18n.GL3082(),i18n.GL3083(),i18n.GL3084()};
 	String[] subjects = new String[]{i18n.GL1000(),i18n.GL1001(),i18n.GL1002(),i18n.GL1003(),i18n.GL3085(),i18n.GL3086()};
 
 	/**
@@ -163,10 +163,16 @@ public class PreFilterPopup extends PopupPanel {
 		// TODO Auto-generated method stub
 		for(int i=0;i<stringArray.length;i++){
 			gradeCheckBox= new CheckBox();
-			/*if(stringArray.equals(elementaryGrades)){
-				gradeCheckBox.setName("K");
-			}*/
 			gradeCheckBox.setName(stringArray[i]);
+			
+			if(stringArray.equals(higherGrades)){
+				if(stringArray[i].equals(i18n.GL3084())){
+					gradeCheckBox.setName("12gte");
+				}
+				/*if(stringArray[i].equals(i18n.GL3084())){
+					gradeCheckBox.setName("13gte");
+				}*/
+			}
 			gradeCheckBox.setText(stringArray[i]);
 			gradeCheckBox.setStyleName(CssTokens.FILTER_CHECKBOX);
 			htmlPanel.add(gradeCheckBox);
