e0c002e250cdc951a5f933c448a88cbf3ff0f860
Sushma <ssushma@goorulearning.org>
2014-12-17 18:58:27 +0530

    After remixing a colleciton, does not take user to My Colleciton page - Issue Resolved

--- src/main/java/org/ednovo/gooru/client/mvp/play/collection/CollectionPlayerUiHandlers.java
+++ src/main/java/org/ednovo/gooru/client/mvp/play/collection/CollectionPlayerUiHandlers.java
@@ -25,6 +25,7 @@
 package org.ednovo.gooru.client.mvp.play.collection;
 
 import org.ednovo.gooru.client.gin.BaseUiHandlers;
+import org.ednovo.gooru.client.mvp.play.collection.end.study.CloseCollectionPlayerHandler;
 import org.ednovo.gooru.client.mvp.play.collection.event.ShowCollectionTabWidgetEventHandler;
 import org.ednovo.gooru.client.mvp.play.collection.event.ShowResourceViewEventHandler;
 import org.ednovo.gooru.client.mvp.play.collection.event.UpdateCollectionViewCountEventHandler;
@@ -32,7 +33,7 @@ import org.ednovo.gooru.client.mvp.rating.events.UpdateFlagIconColorEventHandler
 import org.ednovo.gooru.client.mvp.shelf.event.RefreshCollectionInShelfListInPlayHandler;
 
 public interface CollectionPlayerUiHandlers extends BaseUiHandlers,ShowResourceViewEventHandler,UpdateCollectionViewCountEventHandler,
-					ShowCollectionTabWidgetEventHandler,RefreshCollectionInShelfListInPlayHandler,UpdateFlagIconColorEventHandler{
+					ShowCollectionTabWidgetEventHandler,RefreshCollectionInShelfListInPlayHandler,UpdateFlagIconColorEventHandler,CloseCollectionPlayerHandler{
 	public void updateResourceThumbsRating(int userThumbsRataing);
 	public void showLoginPopupWidget(String widgetMode);
 	public void resetCollectionPlayer();
