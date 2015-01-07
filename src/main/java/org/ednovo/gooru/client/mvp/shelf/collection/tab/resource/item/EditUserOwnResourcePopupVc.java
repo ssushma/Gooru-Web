3959786bda2d9b2927ede9064fbb25f1755a1f45
sravanthi <sravanthi@goorulearning.org>
2015-01-07 11:48:13 +0530

    "updated accesshazard text."

--- src/main/java/org/ednovo/gooru/client/mvp/shelf/collection/tab/resource/item/EditUserOwnResourcePopupVc.java
+++ src/main/java/org/ednovo/gooru/client/mvp/shelf/collection/tab/resource/item/EditUserOwnResourcePopupVc.java
@@ -822,20 +822,20 @@ public abstract class EditUserOwnResourcePopupVc extends AppPopUp implements Sel
 		accessHazard.getElement().setAttribute("title",i18n.GL1804());
 		accessHazard.getElement().getStyle().setDisplay(Display.INLINE_BLOCK);
 		
-		flashingHazard.setText(i18n.GL1806());
+		flashingHazard.setText(i18n.GL3110());
 		flashingHazard.getElement().setId("lblFlashingHazard");
-		flashingHazard.getElement().setAttribute("alt",i18n.GL1806());
-		flashingHazard.getElement().setAttribute("title",i18n.GL1806());
+		flashingHazard.getElement().setAttribute("alt",i18n.GL3110());
+		flashingHazard.getElement().setAttribute("title",i18n.GL3110());
 		
-		motionSimulationHazard.setText(i18n.GL1808());
+		motionSimulationHazard.setText(i18n.GL3111());
 		motionSimulationHazard.getElement().setId("lblMotionSimulationHazard");
-		motionSimulationHazard.getElement().setAttribute("alt",i18n.GL1808());
-		motionSimulationHazard.getElement().setAttribute("title",i18n.GL1808());
+		motionSimulationHazard.getElement().setAttribute("alt",i18n.GL3111());
+		motionSimulationHazard.getElement().setAttribute("title",i18n.GL3111());
 		
-		soundHazard.setText(i18n.GL1810());
+		soundHazard.setText(i18n.GL3112());
 		soundHazard.getElement().setId("lblSoundHazard");
-		soundHazard.getElement().setAttribute("alt",i18n.GL1810());
-		soundHazard.getElement().setAttribute("title",i18n.GL1810());
+		soundHazard.getElement().setAttribute("alt",i18n.GL3112());
+		soundHazard.getElement().setAttribute("title",i18n.GL3112());
 		
 
 		mediaLabel.setText("Media Feature");
@@ -2360,13 +2360,13 @@ public abstract class EditUserOwnResourcePopupVc extends AppPopUp implements Sel
 		
 		String[] stringArry=accessHazardStr.split(" : ");
 		if(stringArry.length!=0){
-			if(stringArry[1].trim().equalsIgnoreCase(i18n.GL1806().trim())){
+			if(stringArry[1].trim().equalsIgnoreCase(i18n.GL3110().trim())){
 				flashingHazard.getElement().addClassName(AddTagesCBundle.INSTANCE.css().select());
 			}
-			if(stringArry[1].trim().equalsIgnoreCase(i18n.GL1808().trim())){
+			if(stringArry[1].trim().equalsIgnoreCase(i18n.GL3111().trim())){
 				motionSimulationHazard.getElement().addClassName(AddTagesCBundle.INSTANCE.css().select());
 			}
-			if(stringArry[1].trim().equalsIgnoreCase(i18n.GL1810().trim())){
+			if(stringArry[1].trim().equalsIgnoreCase(i18n.GL3112().trim())){
 				soundHazard.getElement().addClassName(AddTagesCBundle.INSTANCE.css().select());
 			}
 			setAdvancedAccessHazardStyles(stringArry.length);
