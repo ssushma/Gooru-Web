19f05ed36e041a2609d81ad534bf148d40419c56
Sushma <ssushma@goorulearning.org>
2015-01-19 16:48:46 +0530

    hr not available issue resolved

--- src/main/java/org/ednovo/gooru/client/mvp/analytics/util/AnalyticsUtil.java
+++ src/main/java/org/ednovo/gooru/client/mvp/analytics/util/AnalyticsUtil.java
@@ -136,9 +136,9 @@ public class AnalyticsUtil {
 		}
 		if(hours!=0){
 			if(createdTime!=null){
-				createdTime=createdTime+hours+" ";
+				createdTime=createdTime+hours+"hr ";
 			}else{
-				createdTime=hours+" ";
+				createdTime=hours+"hr ";
 			}
 		}
 		if(minutes!=0){
