4d74d68c48e7b5d3ad91a8936abe4dce713a3770
Sravanthi <sravanthi@goorulearning.org>
2015-05-15 16:09:30 +0530

    1.4 QA [Search] Resource name is not displayed when user hover the mouse on resource thumbnail implemented

--- src/main/java/org/ednovo/gooru/client/mvp/search/util/ResourceImageWidget.java
+++ src/main/java/org/ednovo/gooru/client/mvp/search/util/ResourceImageWidget.java
@@ -1,16 +1,22 @@
 package org.ednovo.gooru.client.mvp.search.util;
 
+import org.ednovo.gooru.client.uc.tooltip.LibraryTopicCollectionToolTip;
 import org.ednovo.gooru.shared.model.content.ResourceDo;
 import org.ednovo.gooru.shared.util.StringUtil;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ErrorEvent;
 import com.google.gwt.event.dom.client.ErrorHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ResourceImageWidget extends Composite {
@@ -24,6 +30,7 @@ public class ResourceImageWidget extends Composite {
 	
 	@UiField Image imgResourceImg;
 	@UiField HTMLPanel imageOverlay;
+	private PopupPanel toolTipPopupPanel = new PopupPanel();
 	
 	public ResourceImageWidget(final ResourceDo resourceDo) {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -51,6 +58,23 @@ public class ResourceImageWidget extends Composite {
 			@Override
 			public void onError(ErrorEvent event) {
 				imgResourceImg.setUrl("../images/default-"+categoryValue+".png");
+			}
+		});
+		imgResourceImg.addMouseOverHandler(new MouseOverHandler() {
+			@Override
+			public void onMouseOver(MouseOverEvent event) {
+				// TODO Auto-generated method stub
+				toolTipPopupPanel.clear();
+				toolTipPopupPanel.setWidget(new LibraryTopicCollectionToolTip(resourceDo.getTitle(),categoryValue,""));
+				toolTipPopupPanel.setStyleName("");
+				toolTipPopupPanel.setPopupPosition(event.getRelativeElement().getAbsoluteLeft() - 2, event.getRelativeElement().getAbsoluteTop() + 55);
+				toolTipPopupPanel.show();
+			}
+		});
+		imgResourceImg.addMouseOutHandler(new MouseOutHandler() {
+			@Override
+			public void onMouseOut(MouseOutEvent event) {
+				toolTipPopupPanel.hide();
 			}
 		});
 		imageOverlay.addStyleName(categoryValue.toLowerCase()+"Small");
