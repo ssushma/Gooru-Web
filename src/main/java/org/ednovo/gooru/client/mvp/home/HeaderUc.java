22e0b8a3498ae65c95e9240e75d233ecd4214450
Sravanthi <sravanthi@goorulearning.org>
2015-05-27 19:06:38 +0530

    color changes handled for goorusearch label

--- src/main/java/org/ednovo/gooru/client/mvp/home/HeaderUc.java
+++ src/main/java/org/ednovo/gooru/client/mvp/home/HeaderUc.java
@@ -358,7 +358,7 @@ public class HeaderUc extends Composite
 	Anchor discoverLink, organizeLink, teachLink, studyLink, loggedInfoLbl;
 
 	@UiField
-	Label thanksLbl,gooruLabel;
+	Label thanksLbl;
 	
 	@UiField
 	static Label arrowLbl;
@@ -414,7 +414,7 @@ public class HeaderUc extends Composite
 			public void keyAction(String text,KeyUpEvent event) {
 				MixpanelUtil.Search_autocomplete_select();
 				autokeySuggestOracle.clear();
-				text.replaceAll("-Gooru Search", "");
+				text.replaceAll("-<n> Gooru Search</n>", "");
 				autoSuggestKeywordDo.setQuery(text);
 				searchData = getEditSearchTxtBox().getText();
 				autoSuggestKeywordDo.setType("resource");
@@ -437,7 +437,7 @@ public class HeaderUc extends Composite
 			
 			@Override
 			public void onSelection(final SelectionEvent<Suggestion> event) {
-				final String searchText = event.getSelectedItem().getDisplayString().replaceAll("-Gooru Search", "");
+				final String searchText = event.getSelectedItem().getDisplayString().replaceAll("-<n> Gooru Search</n>", "");
 				editSearchTxtBox.setText(searchText.trim());
 				GWT.runAsync(new SimpleRunAsyncCallback() {
 					@Override
@@ -459,7 +459,7 @@ public class HeaderUc extends Composite
 								String queryVal = params.get("query");
 								// queryVal = queryVal.replaceAll("%5C1", "&");
 								Map<String, String> map = params;
-								queryVal.replaceAll("-Gooru Search", "");
+								queryVal.replaceAll("-<n> Gooru Search</n>", "");
 								map.put("query", queryVal);
 								editSearchTxtBox.setText(queryVal);
 								AppClientFactory.getPlaceManager().revealPlace(
@@ -500,7 +500,6 @@ public class HeaderUc extends Composite
 			}
 		});
 		initWidget(uiBinder.createAndBindUi(this));
-		gooruLabel.setVisible(false);
 		headerMainPanel.getElement().setAttribute("id", "headerMainPanel");
 
 		logoutPanelVc = new LogoutPanelVc();
@@ -818,7 +817,7 @@ public class HeaderUc extends Composite
 				int key=event.getNativeEvent().getKeyCode();
 				if(key==KeyCodes.KEY_ENTER){
 					String searchText = editSearchTxtBox.getText();
-					searchText = searchText.replaceAll("-Gooru Search", "");
+					searchText = searchText.replaceAll("-<n> Gooru Search</n>", "");
 					editSearchTxtBox.setText(searchText.trim());
 				}
 			}
@@ -1647,7 +1646,7 @@ public class HeaderUc extends Composite
 
 			}
 		}
-		params.put("query", getEditSearchText().replaceAll("-Gooru Search", ""));
+		params.put("query", getEditSearchText().replaceAll("-<n> Gooru Search</n>", ""));
 		String currentPlaceToken=AppClientFactory.getPlaceManager().getCurrentPlaceRequest().getNameToken();
 		AppClientFactory.printInfoLogger("Header-updateparams::"+currentPlaceToken);
 		String collectionType = AppClientFactory.getPlaceManager().getRequestParameter(IsGooruSearchView.COLLECTIONTYPE_FLT,null);
@@ -2094,9 +2093,7 @@ public class HeaderUc extends Composite
 			SearchDo<AutoSuggestKeywordSearchDo> autoSuggestKeywordDo) {
 		autokeySuggestOracle.clear();
 		this.autoSuggestKeywordDo = autoSuggestKeywordDo;
-		gooruLabel.setText(i18n.GL3278());
-		String gooruSearchTextLbl = gooruLabel.getText();
-		autokeySuggestOracle.add(searchData+gooruSearchTextLbl);
+		autokeySuggestOracle.add(searchData+i18n.GL0146());
 		if (this.autoSuggestKeywordDo.getSearchResults() != null) {
 			for (AutoSuggestKeywordSearchDo autoSuggestKeywordSearchDo : autoSuggestKeywordDo
 					.getSearchResults()) {
@@ -2205,7 +2202,7 @@ public class HeaderUc extends Composite
 
 	private void changeQueryParams() {
 		String searchText = editSearchTxtBox.getText();
-		searchText= searchText.replaceAll("-Gooru Search", "");
+		searchText= searchText.replaceAll("-<n> Gooru Search</n>", "");
 		editSearchTxtBox.setText(searchText.trim());
 		Window.enableScrolling(true);
 		AppClientFactory.fireEvent(new SetHeaderZIndexEvent(0, true));
