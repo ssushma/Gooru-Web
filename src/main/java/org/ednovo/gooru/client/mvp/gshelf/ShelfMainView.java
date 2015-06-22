17ca427e71c3d7eb97eb4a1b93dbecd4c069ac75
janamitra <janamithra@goorulearning.org>
2015-06-22 17:50:23 +0530

    Creating new collection/folder template left side and handled scroll height issue.

--- src/main/java/org/ednovo/gooru/client/mvp/gshelf/ShelfMainView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/gshelf/ShelfMainView.java
@@ -85,7 +85,7 @@ public class ShelfMainView extends BaseViewWithHandlers<ShelfMainUiHandlers> imp
 	
 	@UiField HTMLPanel floderTreeContainer,gShelfMainContainer,pnlSlot,pnlNoDataContainer,pnlMainContainer;
 	
-	@UiField HTMLEventPanel organizeRootPnl;
+	@UiField HTMLEventPanel organizeRootPnl,createNewPnl;
 	
 	@UiField Button btnSelectedText;
 	
@@ -108,6 +108,8 @@ public class ShelfMainView extends BaseViewWithHandlers<ShelfMainUiHandlers> imp
 	private static final String COURSE = i18n.GL0574();
 	
 	private static final String COLLECTION = i18n.GL0645();
+	
+	private String VIEW ="view";
 	
 	static Integer pageNumber = 1;
 
@@ -220,7 +222,7 @@ public class ShelfMainView extends BaseViewWithHandlers<ShelfMainUiHandlers> imp
 		}
 		collectionListScrollpanel.getElement().getStyle().setMarginRight(0, Unit.PX);
 		collectionListScrollpanel.getElement().getStyle().setWidth(235, Unit.PX);
-		collectionListScrollpanel.getElement().getStyle().setHeight(Window.getClientHeight(), Unit.PX);
+		collectionListScrollpanel.getElement().getStyle().setHeight(Window.getClientHeight()-181, Unit.PX);
 	}
 	
 	/**
@@ -232,10 +234,10 @@ public class ShelfMainView extends BaseViewWithHandlers<ShelfMainUiHandlers> imp
 		lnkMyCourses.getElement().setId("lnkMyCourses");
 		lnkMyFolders.getElement().setId("lnkMyFolders");
 		lnkMyCollections.getElement().setId("lnkMyCollections");
+		StringUtil.setAttributes(createNewPnl.getElement(), "createNew", "createNew", "createNew");
 		StringUtil.setAttributes(organizeRootPnl.getElement(), "organizeRootPnl", "", "");
 		StringUtil.setAttributes(organizelbl.getElement(), "organizelbl", "", "");
 		StringUtil.setAttributes(collectionListScrollpanel.getElement(), "FoldersListScrollpanel", "", "");
-		
 	}
 	private void setTreeStucture() {
 		floderTreeContainer.clear();
@@ -520,6 +522,15 @@ public class ShelfMainView extends BaseViewWithHandlers<ShelfMainUiHandlers> imp
 		AppClientFactory.getPlaceManager().revealPlace(PlaceTokens.MYCOLLECTION);
 	}
 	
+	@UiHandler("createNewPnl")
+	public void createNewCourseOrCollection(ClickEvent event) {
+		ShelfTreeWidget shelfTreeWidget = new ShelfTreeWidget(null, 1);
+		TreeItem treeItem = new TreeItem(shelfTreeWidget);
+		//shelfTreeWidget.setWidgetPositions(1, 0, null);
+		shelfFolderTree.insertItem(0, treeItem);
+		correctStyle(treeItem);
+	}
+	
 	/**
 	 * To get more collection item after scroll down,if collection is more than 20.
 	 * @param event instance of ScrollEvent
@@ -570,5 +581,11 @@ public class ShelfMainView extends BaseViewWithHandlers<ShelfMainUiHandlers> imp
    			pnlNoDataContainer.setVisible(false);
    			Window.enableScrolling(false);
    		}
+   	}
+   	/**
+   	 * @return viewType
+   	 */
+   	public String getViewType(){
+		return AppClientFactory.getPlaceManager().getRequestParameter(VIEW);
    	}
 }
