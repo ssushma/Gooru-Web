843950b1453a76f55a6b86d3de97fcac4c725747
janamitra <janamithra@goorulearning.org>
2015-07-08 14:49:18 +0530

    generating share links and handled functionality in share page

--- src/main/java/org/ednovo/gooru/client/mvp/gshelf/collectiondetails/CollectionShareTabView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/gshelf/collectiondetails/CollectionShareTabView.java
@@ -24,13 +24,23 @@
  ******************************************************************************/
 package org.ednovo.gooru.client.mvp.gshelf.collectiondetails;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.ednovo.gooru.application.client.gin.BaseViewWithHandlers;
 import org.ednovo.gooru.application.shared.i18n.MessageProperties;
+import org.ednovo.gooru.shared.util.ClientConstants;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
@@ -41,12 +51,25 @@ import com.google.inject.Inject;
 public class CollectionShareTabView extends BaseViewWithHandlers<CollectionShareTabUiHandlers> implements IsCollectionShareTabView {
 
 	private static CollectionShareTabViewUiBinder uiBinder = GWT.create(CollectionShareTabViewUiBinder.class);
-	
+
 	@UiTemplate("CollectionShareTabView.ui.xml")
 	interface CollectionShareTabViewUiBinder extends UiBinder<Widget, CollectionShareTabView> {
 	}	
-	
+
+	@UiField TextArea shareTextArea;
+	@UiField Anchor bitlyLink,embedLink;
+
+
 	public MessageProperties i18n = GWT.create(MessageProperties.class);
+
+	private String shareUrl="";
+
+	private String shareBitlyUrl="";
+
+	private String embedurl="";
+	private String embedBitlyUrl="";
+
+	private Map<String, String> collectionShareMap=null;
 
 	/**
 	 * Class constructor 
@@ -56,5 +79,88 @@ public class CollectionShareTabView extends BaseViewWithHandlers<CollectionShare
 	public CollectionShareTabView() {
 		setWidget(uiBinder.createAndBindUi(this));
 	}
-	
+
+	@Override
+	public void setCollectionShareData(Map<String, String> shareUrlsList){
+		collectionShareMap=new HashMap<String,String>();
+		if(shareUrlsList!=null){
+			shareBitlyUrl = shareUrlsList.get(ClientConstants.SHORTENURL).toString();
+			//shareUrl= shareUrlsList.get(ClientConstants.DECODERAWURL).toString();
+			embedurl=shareUrlsList.get(ClientConstants.EMBEDURLRAWURL).toString();
+			setIframeUrl(embedurl);		
+		}
+		collectionShareMap.put(i18n.GL0643(), shareUrl);
+		collectionShareMap.put(i18n.GL0639(), shareBitlyUrl);
+		embedLink.setText(i18n.GL0640());
+		embedLink.getElement().setAttribute("alt",i18n.GL0640());
+		embedLink.getElement().setAttribute("title",i18n.GL0640());
+
+		bitlyLink.setText(i18n.GL0639());
+		bitlyLink.getElement().setAttribute("alt",i18n.GL0639());
+		bitlyLink.getElement().setAttribute("title",i18n.GL0639());
+
+		shareTextArea.setText(shareUrl);
+		shareTextArea.getElement().setAttribute("alt",shareUrl);
+		shareTextArea.getElement().setAttribute("title",shareUrl);
+	}
+
+	public void setIframeUrl(String iframeBitlyUrl){
+		embedBitlyUrl = "<iframe width=\"1024px\" height=\"768px\" src=\"" + iframeBitlyUrl + "\" frameborder=\"0\" ></iframe>";
+		collectionShareMap.put(i18n.GL0640(), embedBitlyUrl);
+	}
+
+	@UiHandler("embedLink")
+	public void onClickEmbedLink(ClickEvent clickEvent){
+		String shareTxt=shareTextArea.getText();
+		String embed=embedLink.getText();
+		Iterator<String> keyIterator =null;
+
+		keyIterator=collectionShareMap.keySet().iterator();
+
+		while(keyIterator.hasNext()){
+			String key = keyIterator.next();
+			String value = collectionShareMap.get(key);
+			if(shareTxt.equalsIgnoreCase(value)){
+				embedLink.setText(key);
+				embedLink.getElement().setAttribute("alt",key);
+				embedLink.getElement().setAttribute("title",key);
+				shareTextArea.setText(collectionShareMap.get(embed));
+				shareTextArea.getElement().setAttribute("alt",collectionShareMap.get(embed));
+				shareTextArea.getElement().setAttribute("title",collectionShareMap.get(embed));
+
+			}
+		}
+	}
+
+	@UiHandler("shareTextArea")
+	public void onClickshareTextArea(ClickEvent clickEvent){
+		shareTextArea.selectAll();
+		shareTextArea.setFocus(true);
+	}
+
+	@UiHandler("bitlyLink")
+	public void onClickBitlyLink(ClickEvent clickEvent){
+		String shareTxt=shareTextArea.getText();
+		String linkUrl=bitlyLink.getText();
+		Iterator<String> keyIterator =null;
+		keyIterator=collectionShareMap.keySet().iterator();
+		while(keyIterator.hasNext()){
+			String key = keyIterator.next();
+			String value= collectionShareMap.get(key);
+			if(shareTxt.equalsIgnoreCase(value)){
+				bitlyLink.setText(key);
+				bitlyLink.getElement().setAttribute("alt",key);
+				bitlyLink.getElement().setAttribute("title",key);
+				shareTextArea.setText(collectionShareMap.get(linkUrl));
+				shareTextArea.getElement().setAttribute("alt",collectionShareMap.get(linkUrl));
+				shareTextArea.getElement().setAttribute("title",collectionShareMap.get(linkUrl));
+			}
+		}
+	}
+
+	@Override
+	public void setShareUrl(String shareUrl) {
+		this.shareUrl=shareUrl;
+	}
+
 }
