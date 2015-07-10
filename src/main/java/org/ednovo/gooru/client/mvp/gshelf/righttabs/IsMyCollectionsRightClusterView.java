eacaae0f376032e1ab512031192cdb3f9da3a884
sravanthi <sravanthi@goorulearning.org>
2015-07-10 16:08:01 +0530

    Removed unused imports

--- src/main/java/org/ednovo/gooru/client/mvp/gshelf/righttabs/IsMyCollectionsRightClusterView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/gshelf/righttabs/IsMyCollectionsRightClusterView.java
@@ -29,8 +29,6 @@ import java.util.HashMap;
 import org.ednovo.gooru.application.client.gin.IsViewWithHandlers;
 import org.ednovo.gooru.application.shared.model.folder.FolderDo;
 
-import com.google.gwt.user.client.ui.HTMLPanel;
-
 /**
  * @author Search Team
  *
