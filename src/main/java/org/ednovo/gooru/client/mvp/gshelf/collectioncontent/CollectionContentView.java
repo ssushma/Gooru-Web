226e79470adc852e5016148b4509312a79881d1e
Sushma <ssushma@goorulearning.org>
2015-07-23 19:12:47 +0530

    Empty screen integrated for collection and assessment content

--- src/main/java/org/ednovo/gooru/client/mvp/gshelf/collectioncontent/CollectionContentView.java
+++ src/main/java/org/ednovo/gooru/client/mvp/gshelf/collectioncontent/CollectionContentView.java
@@ -39,6 +39,8 @@ import org.ednovo.gooru.application.shared.model.content.CollectionItemDo;
 import org.ednovo.gooru.application.shared.model.content.CollectionQuestionItemDo;
 import org.ednovo.gooru.application.shared.model.folder.FolderDo;
 import org.ednovo.gooru.client.effects.FadeInAndOut;
+import org.ednovo.gooru.client.mvp.gshelf.courselist.EmptyAssessmentView;
+import org.ednovo.gooru.client.mvp.gshelf.courselist.EmptyCollectionView;
 import org.ednovo.gooru.client.mvp.gshelf.util.ContentResourceWidgetWithMove;
 import org.ednovo.gooru.client.mvp.search.event.SetHeaderZIndexEvent;
 import org.ednovo.gooru.client.mvp.shelf.collection.tab.resource.item.EditQuestionPopupVc;
@@ -87,7 +89,7 @@ public class CollectionContentView extends BaseViewWithHandlers<CollectionConten
 			UiBinder<Widget, CollectionContentView> {
 	}
 
-	@UiField HTMLPanel pnlContentContainer;
+	@UiField HTMLPanel pnlContentContainer,emptyContainerdiv;
 	@UiField VerticalPanel pnlReosurceList;
 	@UiField Button btnAddResources, btnAddQuestions;
 	@UiField Anchor ancAddResource, ancAddQuestion;
@@ -146,6 +148,7 @@ public class CollectionContentView extends BaseViewWithHandlers<CollectionConten
 	public void setData(CollectionDo listOfContent,FolderDo folderDo, RefreshType type){
 		this.listOfContent = listOfContent;
 		this.folderDo = folderDo;
+		emptyContainerdiv.clear();
 		if (AppClientFactory.isContentAdmin() || listOfContent
 				.getUser().getGooruUId().equals(AppClientFactory.getLoggedInUser()
 						.getGooruUId())){
@@ -157,8 +160,6 @@ public class CollectionContentView extends BaseViewWithHandlers<CollectionConten
 			if (listOfContent.getMeta()!=null /*&& listOfContent.getMeta().getCollaboratorCount() > 0 */&& listOfContent.getLastModifiedUser() != null){
 				String lastModifiedDate = listOfContent.getLastModified().toString() != null ? getTimeStamp(listOfContent.getLastModified().getTime()+"") : "";
 				String lastModifiedUser = listOfContent.getLastModifiedUser().getUsername() != null ?  listOfContent.getLastModifiedUser().getUsername() : "";
-				System.out.println("lastModifiedDate:"+lastModifiedDate);
-				System.out.println("lastModifiedUser:"+lastModifiedUser);
 				//lblLastEditedBy.setText(StringUtil.generateMessage(i18n.GL1112(), lastModifiedDate, lastModifiedUser));
 				//lblLastEditedBy.setVisible(lastModifiedUser!=null && !lastModifiedUser.equalsIgnoreCase("") ? true : false);
 				if (lastModifiedUser!=null && !lastModifiedUser.equalsIgnoreCase("")){
@@ -193,6 +194,26 @@ public class CollectionContentView extends BaseViewWithHandlers<CollectionConten
 			setLastWidgetArrowVisiblity(false);
 		}else{
 			pnlReosurceList.clear();
+			if(folderDo!=null && folderDo.getType()!=null)
+			{
+			if(folderDo.getType().equalsIgnoreCase("collection")){
+			EmptyCollectionView emptyColl = new EmptyCollectionView();
+			emptyColl.getAddResourceBtn().addClickHandler(new NewResourceClickEvent());
+			emptyColl.getAddQuestionBtn().addClickHandler(new NewQuestionClickEvent());
+			emptyContainerdiv.add(emptyColl);
+			}
+			else if(folderDo.getType().equalsIgnoreCase("assessment"))
+			{
+				EmptyAssessmentView emptyAssessment = new EmptyAssessmentView();
+				emptyAssessment.getAddQuestionBtn().addClickHandler(new NewQuestionClickEvent());
+				emptyContainerdiv.add(emptyAssessment);
+			}
+			}
+			else
+			{
+				emptyContainerdiv.clear();
+			}
+			Window.enableScrolling(true);
 		}
 	}
 
