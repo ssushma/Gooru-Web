6606c885b45b0e7102f8c3a68f24cb49c336805f
surya <suryanarayana@goorulearning.org>
2015-07-30 14:29:26 +0530

    Edited Resource format is not displayed at content page(Needs reload)-fixed

--- src/main/java/org/ednovo/gooru/application/server/service/ResourceServiceImpl.java
+++ src/main/java/org/ednovo/gooru/application/server/service/ResourceServiceImpl.java
@@ -972,7 +972,7 @@ public class ResourceServiceImpl extends BaseServiceImpl implements ResourceServ
 			throws GwtException {
 		try{
 
-			JsonRepresentation jsonRep = null;
+			JsonRepresentation jsonRep = null,jsonRepGet=null;
 			String url =null;
 			url = UrlGenerator.generateUrl(getRestEndPoint(), UrlToken.UPDATE_RESOURCE_INFO,collectionItemDo.getParentGooruOid(), collectionItemDo.getCollectionItemId());
 
@@ -1070,8 +1070,11 @@ public class ResourceServiceImpl extends BaseServiceImpl implements ResourceServ
 			getLogger().info("--- pay load -- "+form);
 			JsonResponseRepresentation jsonResponseRep = ServiceProcessor.put(url, getRestUsername(), getRestPassword(),form);
 			jsonRep = jsonResponseRep.getJsonRepresentation();
-			return deserializeCollectionItem(jsonRep);
-
+		
+			JsonResponseRepresentation jsonResponseRep1 = ServiceProcessor.get(url, getRestUsername(), getRestPassword());
+			getLogger().info("---url -- "+url);
+			jsonRepGet=jsonResponseRep1.getJsonRepresentation();
+			return deserializeCollectionItem(jsonRepGet);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
