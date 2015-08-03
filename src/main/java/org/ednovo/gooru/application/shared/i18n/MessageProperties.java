5a52d33164ca4dd2e8a65371ecd7d5fae34bb6d6
Ravinder V <ravinder@goorulearning.org>
2015-08-03 18:02:53 +0530

    Media scripts added for student tab in teacherview

--- src/main/java/org/ednovo/gooru/application/shared/i18n/MessageProperties.java
+++ src/main/java/org/ednovo/gooru/application/shared/i18n/MessageProperties.java
@@ -3293,6 +3293,7 @@ public interface MessageProperties extends Constants {
 				String GL3450_14();
 				String GL3450_15();
 				String GL3450_16();
+				String GL3450_17();
 
 				String GL3348();
 				String GL3367();
