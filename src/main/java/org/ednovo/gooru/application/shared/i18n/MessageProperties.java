2786406b915f17fafb476dbf96fd95323b6b100e
Chandra <chandra@goorulearning.org>
2015-08-04 10:32:13 +0530

    New media scripts added for My Classes

--- src/main/java/org/ednovo/gooru/application/shared/i18n/MessageProperties.java
+++ src/main/java/org/ednovo/gooru/application/shared/i18n/MessageProperties.java
@@ -3424,6 +3424,12 @@ public interface MessageProperties extends Constants {
 	String GL3469_28();
 	String GL3469_29();
 	String GL3469_30();
+	String GL3469_31();
+	String GL3469_32();
+	String GL3469_33();
+	String GL3469_34();
+	String GL3469_35();
+	String GL3469_36();
 	
 	String GL3471();
 	String	GL3473();
